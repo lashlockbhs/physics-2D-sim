63605b6b7febcf48cec715557086c3287cf32309
szetingchen <111320652+szetingchen@users.noreply.github.com>
2023-01-07 06:04:08 +0000

    fixed some syntax and imported to script

--- projects/physics/public/script.js
+++ projects/physics/public/script.js
@@ -1,2 +1,2 @@
 import { setCanvas, drawFilledCircle, clear, width, height, animate, now, registerOnKeyDown, registerOnclick} from './graphics.js';
-
+import {add2Vectors, vectorMultiply, addNumVectors, sigma, pi, velocity} from './math.js';
