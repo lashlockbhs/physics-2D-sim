c9aa1ed940dfb40dc2ad9b437ba627b68a57f0c4
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2023-01-10 06:55:21 +0000

    started redesigning the shape class with out using "sides"

--- projects/physics/public/script.js
+++ projects/physics/public/script.js
@@ -1,5 +1,5 @@
 import { setCanvas, drawFilledCircle, clear, width, height, animate, now, registerOnKeyDown, registerOnclick, drawFilledRect, drawLine } from './graphics.js';
-import { add2Vectors, vectorMultiply, addNumVectors, sigma, pi, degToRad, radToDeg, mean, geoMean, twoPointAngle, distance, getAcceleration, getVelocity, vector } from './math.js';
+import { add2Vectors, vectorMultiply, addNumVectors, sigma, pi, degToRad, radToDeg, mean, geoMean, twoPointAngle, distance, getAcceleration, getVelocity, vector, twoPointXYDif } from './math.js';
 
 const canvas = document.getElementById('screen');
 setCanvas(canvas)
@@ -22,9 +22,6 @@ const collisions = (shapes) => {
   return collisions;
 }
 
-
-
-
 const getBoundCenter = (points) => {
   const pts = points.concat(points[0])
   const area = (sigma(0, pts.length - 2, i => (pts[i].x * pts[i + 1].y) - (pts[i + 1].x * pts[i].y))) / 2
@@ -43,6 +40,12 @@ const rotate = (cx, cy, x, y, angle) => {
   return [nx, ny];
 }
 
+const drawPoints = (ar, color) => {
+  for (const point of ar) {
+    drawLine(point.x, point.y, point.x + 1, point.y + 1, color, 1)
+  }
+}
+
 class Shape {
   constructor(mass, actingForces, vertices) {
     //this.startingX = vertices[0].x //for draw
@@ -54,19 +57,38 @@ class Shape {
     this.centerY = getBoundCenter(vertices).y
     this.rotation = 0
     this.actingForce = [addNumVectors(actingForces)]
+    this.vertDifs = []//the pos of the verticies relitive to the center, in replacment of the "sides" athgorithm
+    for (const vert of vertices) {
+      this.vertDifs.push(twoPointXYDif(vert, { x: this.centerX, y: this.centerY }))
+    }
+  }
+  //should be called everytime you update this opjects varibles outside of the class
+  updateProperties() {
 
-    this.vertices = 
+    console.log(this.vertices)
+    this.vertices.forEach((e, i) => e = { x: this.centerX + this.vertDifs[i].xDif, y: this.centerY + this.vertDifs[i].yDif })
+
+    this.vertices.forEach((e) => {
+      console.log(e)
+      const rotateCords = rotate(this.centerX, this.centerY, e.x, e.y, this.rotation)
+      e.x = rotateCords[0]
+      e.y = rotateCords[1]
+    })
+    console.log(this.vertices)
   }
   drawShape() {
-    let currX = this.startingX;
-    let currY = this.startingY;
+    console.log(this.vertices.length)
+    drawPoints(this.vertices)
+    for (let i = 0; i < vertices.length; i++) {
 
-    for (let i = 0; i < this.sides.length; i++) {
-      let startPoint = rotate(this.centerX, this.centerY, currX, currY, this.rotation)
-      let endPoint = rotate(this.centerX, this.centerY, currX + this.sides[i].xAdd, currY + this.sides[i].yAdd, this.rotation)
-      drawLine(startPoint[0], startPoint[1], endPoint[0], endPoint[1], 'black');
-      currX = currX + this.sides[i].xAdd;
-      currY = currY + this.sides[i].yAdd;
+      if (i + 1 === this.vertices.length) {
+        drawLine(this.vertices[i].x, this.vertices[i].y, this.vertices[0].x, this.vertices[0].y, 'black', 1)
+        console.log(this.vertices[i].x + ", " + this.vertices[i].y + " to " + this.vertices[0].x + ", " + this.vertices[0].y)
+      }
+      else {
+        drawLine(this.vertices[i].x, this.vertices[i].y, this.vertices[i + 1].x, this.vertices[i + 1].y, 'black', 1)
+        console.log(this.vertices[i].x + ", " + this.vertices[i].y + " to " + this.vertices[i + 1].x + ", " + this.vertices[i + 1].y)
+      }
     }
   }
   getBoundOfObject() {
@@ -126,15 +148,27 @@ registerOnKeyDown(() => {
 })
 console.log("e")
 
+const shape1 = objArray[1]
+
+shape1.rotation = 180;
+shape1.updateProperties()
+
+drawFilledCircle(shape1.centerX, shape1.centerY, 2.5, "red")
+shape1.drawShape();
+
+/*
 let next = 0;
 let countFrame = 0;
 const drawFrame = (time) => {
   if ((time > next) && animateStart) {
     clear();
     for (const shape of objArray) {
+      shape.rotation = 20;
+      shape.updateProperties()
+
       drawFilledCircle(shape.centerX, shape.centerY, 2.5, "red")
       shape.drawShape();
-      shape.rotation = countFrame;
+
       next += 1;
       countFrame++;
     }
@@ -147,3 +181,4 @@ export {
   collisions,
   getBoundCenter,
 }
+*/
