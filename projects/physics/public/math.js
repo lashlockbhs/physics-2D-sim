977745c6a3bc9a780c71c19d00b3fb944aa125c0
szetingchen <111320652+szetingchen@users.noreply.github.com>
2023-01-27 23:29:34 +0000

    some stuff

--- projects/physics/public/math.js
+++ projects/physics/public/math.js
@@ -1,6 +1,7 @@
 //vector manipulation, stored in radians
-const shapeArea = (vArray) => {
-  return sigma(0, vArray.length - 2, (i) => vArray[i].x * vArray[i + 1].y - vArray[i + 1].x * vArray[i].y) / 2;
+const shapeArea = (vertices) => {
+  const verticeArray = vertices.concat(vertices[0]);
+  return Math.abs(sigma(0, verticeArray.length - 2, (i) => verticeArray[i].x * verticeArray[i + 1].y - verticeArray[i + 1].x * verticeArray[i].y) / 2);
 }
 
 const vector = (degrees, magnitude) => {
---
+++ projects/physics/public/perfectShapes.js
@@ -0,0 +1,25 @@
+import {
+    setCanvas,
+    drawLine,
+    drawCircle,
+    drawRect,
+    drawTriangle,
+    drawFilledCircle,
+    drawFilledRect,
+    drawFilledTriangle,
+    drawRoundedRect,
+    drawFilledRoundedRect,
+    drawText,
+    clear,
+    width,
+    height,
+    now,
+    animate,
+    registerOnclick,
+    registerOnKeyDown,
+  } from './graphics.js';
+
+const drawPerfectCircle = (o1, vector) => {
+return registerOnclick
+}
+  
