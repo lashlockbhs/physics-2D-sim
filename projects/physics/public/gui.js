7b05fef5f7c239a028190227255a72b2db383b37
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2023-01-31 22:27:12 +0000

    drop down triangle

--- projects/physics/public/gui.js
+++ projects/physics/public/gui.js
@@ -16,7 +16,46 @@ class dropMenu {
         this.hidden = hidden;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
-        this.elArray = []
+        this.elArray = [];
+        this.headbar;
+        this.#createElements()
+    }
+    #createElements(){
+        this.headbar = document.createElement("div");
+        this.headbar.setAttribute("id", "headbar");
+        this.headbar.style.width = this.boxWidth + "px";
+        this.headbar.style.height = 10 + "px";
+        this.headbar.style.left = this.xOffset + "px";
+        this.headbar.style.top = this.yOffset + "px";
+        //this.headbar.ondrag
+        this.menuHolder.append(this.headbar);
+
+        const triUp = "&#9650;";
+        const triDown = "&#9660;";
+        let state = "down";
+        const triangle = document.createElement("div")
+        triangle.setAttribute("id", "triangle")
+        triangle.innerHTML = (triDown);
+        this.headbar.append(triangle);
+
+        triangle.onclick = () => 
+        {
+            if(state === "up"){
+                triangle.innerHTML = triDown;
+                console.log(triangle.innerHTML + " down")
+                state = "down"
+                this.showAllEl();
+            }
+            else{
+                triangle.innerHTML = triUp;
+                console.log(triangle.innerHTML + " up")
+                state = "up"
+                this.hideAllEl();
+            }
+        };
+        
+        
+
 
         //create button/option elements as divs
         for (let i = 0; i < this.options.length; i++) {
@@ -27,21 +66,29 @@ class dropMenu {
             let text = document.createTextNode(this.options[i].text);
             div.append(text);
             div.style.fontSize = this.textSize + "px";
-            div.style.position = "relative";
-            div.style.left = this.xOffset + "px";
-            div.style.top = this.yOffset + "px";
-            this.hidden ? div.style.display="none" : div.style.display="grid"
-
+            div.style.top = 10 + "px";
+            div.style.left = -1 + "px";
+            this.hidden ? div.style.visibility = "hidden" : div.style.display = "grid";
+            
 
             div.onmousedown = (e) => { this.onClick(div); e.stopPropagation() };
-            div.onmouseup = (e) => { this.refresh(div); e.stopPropagation()};
+            div.onmouseup = (e) => { this.refresh(div); e.stopPropagation() };
             div.onmousemove = (e) => { this.onHover(div); e.stopPropagation() };
-            div.onmouseleave = (e) => { this.refresh(div); e.stopPropagation()}; //stoppropagation from chatgbt
+            div.onmouseleave = (e) => { this.refresh(div); e.stopPropagation() }; //stoppropagation from chatgbt
 
             this.elArray.push(div);
-            this.menuHolder.append(div);
+            this.headbar.append(div);
 
         }
+        console.log(this.elArray)
+    }
+    hideMenu() {
+        this.headbar.style.visibility = "hidden";
+        this.hideAllEl();
+    }
+    showMenu() {
+        this.headbar.style.visibility = "visible";
+        //this.showAllEl();
     }
     showAllEl() {
         this.elArray.forEach(e => this.#showEl(e));
@@ -50,32 +97,35 @@ class dropMenu {
         this.elArray.forEach(e => this.#hideEl(e));
     }
     #hideEl(el) {
-        el.style.display = "none";
+        el.style.visibility = "hidden"
     }
     #showEl(el) {
-        el.style.display = "grid";
+        el.style.visibility = "visible"
     }
     onClick(el) {
         el.style.backgroundColor = "rgb(105, 102, 102)";
         const child = this.childs.find(e => e.button === el);
-        console.log(child.el.hidden);
-        if(child.el.hidden){
-            child.el.showAllEl();
+        if (child.el.hidden) {
+            child.el.showMenu();
             child.el.hidden = false;
 
         }
-        else{
+        else {
 
-            child.el.hideAllEl();
+            child.el.hideMenu();
             child.el.hidden = true;
         }
     }
     onHover(el) {
         el.style.backgroundColor = "rgb(155, 151, 151)";
     }
+    onDrag(el) {
+
+    }
     refresh(el) {
         el.style.backgroundColor = "rgb(180, 171, 171)";
     }
+    
 }
 
 
@@ -96,12 +146,12 @@ const perfShapesMenu = new dropMenu
         [{ text: "Square" }, { text: "Cirlce" }, { text: "Rect" }, { text: "Triange" }], //menu text
         document.getElementById("menuHolder"), //
         false, //hidden?
-        70, //xoffset
-        -128,  ///yoffset
+        10, //xoffset
+        0,  ///yoffset
         50, //buttonWidth
         10, //buttonheight
         10, //textSize
 
     )
 
-baseMenu.childs.push({el : perfShapesMenu, button : baseMenu.elArray.find(e => e.id === "Perfect Shapes")});
+baseMenu.childs.push({ el: perfShapesMenu, button: baseMenu.elArray.find(e => e.id === "Perfect Shapes") });
