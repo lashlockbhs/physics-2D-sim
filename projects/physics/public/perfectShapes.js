c019f66c067605a6db1f5650397cf4b71ba13b0f
szetingchen <111320652+szetingchen@users.noreply.github.com>
2023-01-31 23:35:56 +0000

    first 2 perfect shapes

--- projects/physics/public/perfectShapes.js
+++ projects/physics/public/perfectShapes.js
@@ -19,7 +19,30 @@ import {
     registerOnKeyDown,
   } from './graphics.js';
 
-const drawPerfectCircle = (o1, vector) => {
-return registerOnclick
-}
+import {
+  Shape
+} from './script.js'
+
+import {
+  shapeArea
+} from './math.js'
+
+const drawPerfectCircle = (o1, radius, vector) => {
+  drawCircle(o1.x, o1.y, radius, 'black', 1);
+};
+
+const drawEquilateralTriangle = (o1, radius, vector) => {
+  new Shape(vector, 
+    [{x: o1.x, y: o1.y + radius}, 
+    {x: o1.x - (Math.sqrt(3) * radius / 2), y: o1.y - (radius / 2)}, 
+    {x: o1.x + (Math.sqrt(3) * radius / 2), y: o1.y - (radius / 2)}], 
+    shapeArea * document.getElementById('density').value);
+};
   
+const drawRegularPerfectQuadrilateral = (o1, radius, vector) => {
+  new Shape(vector,
+    [{x: o1.x + (Math.sqrt(2) / 2 * radius), y: o1.y + (Math.sqrt(2) / 2 * radius)},
+    {x: o1.x + (Math.sqrt(2) / 2 * radius), y: o1.y - (Math.sqrt(2) / 2 * radius)},
+    {x: o1.x - (Math.sqrt(2) / 2 * radius), y: o1.y - (Math.sqrt(2) / 2 * radius)},
+    {x: o1.x - (Math.sqrt(2) / 2 * radius), y: o1.y + (Math.sqrt(2) / 2 * radius)}])
+}
--- projects/physics/public/script.js
+++ projects/physics/public/script.js
@@ -214,6 +214,7 @@ const drawFrame = (time) => {
 animate(drawFrame);
 
 export {
-  canvas
+  canvas,
+  Shape
 }
 
